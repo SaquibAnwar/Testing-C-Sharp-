55331dca29d213f0f2a7c499d2307ad061e70646
Kryptonite <saquibedu@gmail.com>
2020-01-03 16:12:40 +0530

    C# Testing

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -6,8 +6,27 @@ namespace helloWorld
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Hello World!");
             Console.ReadLine();
+
+            int x, y;
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            _ = Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name ? ");
+            string firstName;
+            Console.WriteLine("Enter your first name : ");
+            firstName = Console.ReadLine();
+            string lastName;
+            Console.WriteLine("Enter your last Name : ");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + firstName + " " + lastName);
         }
     }
 }
