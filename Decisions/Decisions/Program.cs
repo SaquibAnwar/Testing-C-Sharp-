88ff5e762b218fa22d555e53e36a07d2d39643bc
Kryptonite <saquibedu@gmail.com>
2020-01-03 17:16:37 +0530

    if-else and conditional

--- Decisions/Decisions/Program.cs
+++ Decisions/Decisions/Program.cs
@@ -6,7 +6,35 @@ namespace Decisions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bob's Great Giveaway\n");
+            Console.Write("Enter door 1, 2 or 3 : ");
+            string userValue = Console.ReadLine();
+            string message = "";
+            if (userValue == "1")
+            {
+                message = "You won a new car!";
+            }
+            else if(userValue == "2")
+            {
+                message = "You won a new boat!";
+            }
+            else if(userValue == "3")
+            {
+                message = "You won a new cat!";
+            }
+            else
+            {
+                message = "Enter a valid input";
+            }
+
+            Console.WriteLine(message);
+
+
+            int x = 7;
+            string result = (x % 2 == 0) ? "even" : "odd";
+            Console.Write("The number " + x + " is ");
+            Console.Write(result);
+            Console.Write(".");
         }
     }
 }
